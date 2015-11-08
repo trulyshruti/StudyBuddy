313b67c7de6d2a585f8caa1e1751355c62387760
Shruti Kulkarni <sgk2118@columbia.edu>
2015-11-08 03:53:11 -0500

    form

--- pages/newgroup.php
+++ pages/newgroup.php
@@ -14,25 +14,51 @@
 	        <h2 class="section-heading">&nbsp;</h2>
 	        <form role="form" action="backend/login.php" method="post">
 	            <div class="form-group">
-	                <label for="name">Name:</label>
-	              <input type="name" class="form-control" id="name" name="name">
+	                <label for="Group-Name">Group Name:</label>
+	              <input type="Group-Name" class="form-control" id="Group-Name" name="Group-Name">
 	            </div>
-	            <div class="form-group">
-	               <label for="Description">Description:</label>
-	             <input type="Description" class="form-control" id="password" name="Description">
-	            </div>
-	                        <div class="form-group">
+	             <div class="form-group">
 	               <label for="Subject">Subject:</label>
 	             <input type="Subject" class="form-control" id="Subject" name="Subject">
 	            </div>
-	          
+	            <div class="form-group">
+	               <label for="Description">Description:</label>
+	             <input type="Description" class="form-control" id="Description" name="Description">
+	            </div>
+	            <div class="form-group">
+	               <label for="Date-Time">Date/Time:</label>
+	             <input type="Date-Time" class="form-control" id="Date-Time" name="Date-Time">
+	              <div class='input-group date' id='datetimepicker1'>
+                    <!-- <input type='text' class="form-control" /> --><br />
+                    <span class="input-group-addon">
+                        <span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                 </div>
+	            </div>
+	            <div class="form-group">
+	               <label for="Days">Days:</label>
+	             <input type="Days" class="form-control" id="Days" name="Days">
+	            </div>
+	            <div class="form-group">
+	               <label for="Location">Location:</label>
+	             <input type="Location" class="form-control" id="Location" name="Location">
+	            </div>
+	            <!-- BEGIN TAGS FORM INPUT --
 	            <div class="form-group">
 	               <label for="Tags">Tags:</label>
 	             <input type="Tags" class="form-control" id="Tags" name="Tags">
 	            </div>
+	        	END TAGS FORM INPUT -->
 	            <button type="submit" class="btn btn-default">Submit</button>
 	        </form>
 	      </div>
+	      <script type="text/javascript">
+            $(function () {
+                $('#datetimepicker1').datetimepicker({
+                    // locale: 'ru'
+                });
+            });
+          </script>
 	     </div>
 	    </div>
 	</div>
