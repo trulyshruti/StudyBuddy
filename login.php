ea031d7c551965599c5a39848cd6d2a5c8e993d2
Shruti Kulkarni <sgk2118@columbia.edu>
2015-11-07 20:52:49 -0500

    make login.php look cooler

--- login.php
+++ login.php
@@ -74,20 +74,27 @@
 
 <a  name="login-form"></a>
 <div class="content-section-a">
-    <form role="form" action="backend/login.php" method="post">
-  <div class="form-group">
-    <label for="username">Username:</label>
-    <input type="username" class="form-control" id="username" name="username">
-  </div>
-  <div class="form-group">
-    <label for="password">Password:</label>
-    <input type="password" class="form-control" id="password" name="password">
-  </div>
-  <div class="checkbox">
-    <label><input type="checkbox"> Remember me</label>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
+    <div class="container">
+     <div class="row">
+      <div class="col-lg-5 col-sm-6">
+        <h2 class="section-heading">Log In</h2>
+        <form role="form" action="backend/login.php" method="post">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="username" class="form-control" id="username" name="username">
+            </div>
+            <div class="form-group">
+               <label for="password">Password:</label>
+             <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <div class="checkbox">
+                <label><input type="checkbox"> Remember me</label>
+           </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+      </div>
+     </div>
+    </div>
 </div>
 
 </body>
