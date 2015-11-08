7d30762cbce6b4adc1fdfbc4688db5273b9e26fa
bwaiharo <benedict.waiharo@rutgers.edu>
2015-11-07 22:18:16 -0500

    Form Update

--- form.php
+++ form.php
@@ -77,19 +77,25 @@
     <div class="container">
      <div class="row">
       <div class="col-lg-5 col-sm-6">
-        <h2 class="section-heading">Log In</h2>
+        <h2 class="section-heading">FORM NAME</h2>
         <form role="form" action="backend/login.php" method="post">
-            <div class="form-group">
-                <label for="username">Username:</label>
-                <input type="username" class="form-control" id="username" name="username">
+        <div class="form-group">
+            <label for="name">Name:</label>
+              <input type="name" class="form-control" id="name" name="name">
             </div>
             <div class="form-group">
-               <label for="password">Password:</label>
-             <input type="password" class="form-control" id="password" name="password">
+               <label for="Description">Description:</label>
+             <input type="Description" class="form-control" id="password" name="Description">
             </div>
-            <div class="checkbox">
-                <label><input type="checkbox"> Remember me</label>
-           </div>
+                        <div class="form-group">
+               <label for="Subject">Subject:</label>
+             <input type="Subject" class="form-control" id="Subject" name="Subject">
+            </div>
+          
+            <div class="form-group">
+               <label for="Tags">Tags:</label>
+             <input type="Tags" class="form-control" id="Tags" name="Tags">
+            </div>
             <button type="submit" class="btn btn-default">Submit</button>
         </form>
       </div>
