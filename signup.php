17a8bab259b635ac79050811a44a408d78c7a90f
Shruti Kulkarni <sgk2118@columbia.edu>
2015-11-07 23:01:27 -0500

    signup form, components- navbar

--- signup.php
+++ signup.php
@@ -77,8 +77,20 @@
     <div class="container">
      <div class="row">
       <div class="col-lg-5 col-sm-6">
-        <h2 class="section-heading">Log In</h2>
-        <form role="form" action="backend/login.php" method="post">
+        <h2 class="section-heading">Sign Up</h2>
+        <form role="form" action="backend/signup.php" method="post">
+            <div class="form-group">
+               <label for="first_name">First Name:</label>
+             <input type="first_name" class="form-control" id="first_name" name="first_name">
+            </div>
+            <div class="form-group">
+               <label for="last_name">Last Name:</label>
+             <input type="last_name" class="form-control" id="last_name" name="last_name">
+            </div>
+            <div class="form-group">
+               <label for="email">Email:</label>
+             <input type="email" class="form-control" id="email" name="email">
+            </div>
             <div class="form-group">
                 <label for="username">Username:</label>
                 <input type="username" class="form-control" id="username" name="username">
@@ -87,9 +99,6 @@
                <label for="password">Password:</label>
              <input type="password" class="form-control" id="password" name="password">
             </div>
-            <div class="checkbox">
-                <label><input type="checkbox"> Remember me</label>
-           </div>
             <button type="submit" class="btn btn-default">Submit</button>
         </form>
       </div>
